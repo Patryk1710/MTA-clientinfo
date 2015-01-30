1e2614bd41cc662e6ffb47dc2472b7c171987a6e
Patryk1710 <newbanan2@gmail.com>
2015-01-30 22:41:33 +0100

    Updated client.lua file

--- clientinfo/client.lua
+++ clientinfo/client.lua
@@ -1,10 +1,10 @@
 ﻿local sx, sy = guiGetScreenSize();
-local timer = 500;
 local info = {};
-local showed = true;
 
-local x = sx - 50;
-local alpha = 100;
+info.showed = false;
+info.timer = 500;
+info.x = sx - 50;
+info.alpha = 100;
 
 local fps;
 info.fps = 0;
@@ -22,28 +22,32 @@ setTimer(function()
 end, 1000, 0);
 
 local function showFunction()
-	local now = getTickCount();
-	local elapsedTime = now - info.startTime;
-	local duration = timer;
-	local progress = elapsedTime / duration;
+	local elapsedTime = getTickCount() - info.startTime;
+	local progress = elapsedTime / info.timer;
 
-	_, alpha, x = interpolateBetween ( 
-		0, alpha, x,
+	_, info.alpha, info.x = interpolateBetween( 
+		0, info.alpha, info.x,
 		0, 200, sx - 350, 
 		progress, 'InOutQuad');
-end;
+
+	if progress >= 1 then
+		removeEventHandler('onClientRender', root, showFunction);
+	end
+end
 
 local function hideFunction()
-	local now = getTickCount();
-	local elapsedTime = now - info.startTime;
-	local duration = timer;
-	local progress = elapsedTime / duration;
+	local elapsedTime = getTickCount() - info.startTime;
+	local progress = elapsedTime / info.timer;
 
-	_, alpha, x = interpolateBetween ( 
-		0, alpha, x, 
+	_, info.alpha, info.x = interpolateBetween( 
+		0, info.alpha, info.x, 
 		0, 100, sx - 50,
 		progress, 'InOutQuad');
-end;
+
+	if progress >= 1 then
+		removeEventHandler('onClientRender', root, hideFunction);
+	end
+end
 
 local function drawInfo()
 	info.posVector = localPlayer.position;
@@ -65,60 +69,60 @@ local function drawInfo()
 		info.zoneString = info.zone..', '..info.city;
 	end;
 
-	dxDrawRectangle(x, sy / 2 - 150, 350, 300, tocolor(30, 30, 30, alpha), false);
-	dxDrawRectangle(x, sy / 2 - 150, 50, 300, tocolor(90, 90, 255, alpha), false);
-	dxDrawImage(x, sy / 2 - 150, 50, 300, 'title.png', 0, 0, 0, tocolor (255, 255, 255, alpha *1.2), false);
+	dxDrawRectangle(info.x, sy / 2 - 150, 350, 300, tocolor(30, 30, 30, info.alpha), false);
+	dxDrawRectangle(info.x, sy / 2 - 150, 50, 300, tocolor(90, 90, 255, info.alpha), false);
+	dxDrawImage(info.x, sy / 2 - 150, 50, 300, 'title.png', 0, 0, 0, tocolor(255, 255, 255, info.alpha *1.2), false);
 
-	dxDrawText(info.zoneString, x + 60, sy / 2 - 140, sx - 10, sy / 2 - 120, tocolor( 255, 255, 255, alpha), 1, 'default', 'left', 'center', true, false, false);
-	dxDrawLine(x + 55, sy / 2 - 121, sx - 10, sy / 2 - 121, tocolor (65, 65, 65, alpha), 1, false);
+	dxDrawText(info.zoneString, info.x + 60, sy / 2 - 140, sx - 10, sy / 2 - 120, tocolor(255, 255, 255, info.alpha), 1, 'default', 'left', 'center', true, false, false);
+	dxDrawLine(info.x + 55, sy / 2 - 121, sx - 10, sy / 2 - 121, tocolor(65, 65, 65, info.alpha), 1, false);
 
-	dxDrawText(math.ceil(info.posVector.x)..', '..math.ceil(info.posVector.y)..', '..math.ceil(info.posVector.z), x + 60, sy / 2 - 120, sx - 10, sy / 2 - 100, tocolor(255, 255, 255, alpha), 1, 'default', 'left', 'center', true, false, false);
-	dxDrawLine(x + 55, sy / 2 - 101, sx - 10, sy / 2 - 101, tocolor (65, 65, 65, alpha), 1, false);
+	dxDrawText(math.ceil(info.posVector.x)..', '..math.ceil(info.posVector.y)..', '..math.ceil(info.posVector.z), info.x + 60, sy / 2 - 120, sx - 10, sy / 2 - 100, tocolor(255, 255, 255, info.alpha), 1, 'default', 'left', 'center', true, false, false);
+	dxDrawLine(info.x + 55, sy / 2 - 101, sx - 10, sy / 2 - 101, tocolor(65, 65, 65, info.alpha), 1, false);
 
-	dxDrawText('Rotation: '..math.ceil(info.rotVector.z)..', Skin: '..localPlayer.model, x + 60, sy / 2 - 100, sx - 10, sy / 2 - 80, tocolor(255, 255, 255, alpha), 1, 'default', 'left', 'center', true, false, false);
-	dxDrawLine(x + 55, sy / 2 - 81, sx - 10, sy / 2 - 81, tocolor (65, 65, 65, alpha), 1, false);
+	dxDrawText('Rotation: '..math.ceil(info.rotVector.z)..', Skin: '..localPlayer.model, info.x + 60, sy / 2 - 100, sx - 10, sy / 2 - 80, tocolor(255, 255, 255, info.alpha), 1, 'default', 'left', 'center', true, false, false);
+	dxDrawLine(info.x + 55, sy / 2 - 81, sx - 10, sy / 2 - 81, tocolor(65, 65, 65, info.alpha), 1, false);
 
-	dxDrawText('Health: '..math.floor(localPlayer.health)..', Armor: '..math.ceil(localPlayer.armor), x + 60, sy / 2 - 80, sx - 10, sy / 2 - 60, tocolor(255, 255, 255, alpha), 1, 'default', 'left', 'center', true, false, false);
-	dxDrawLine(x + 55, sy / 2 - 61, sx - 10, sy / 2 - 61, tocolor (65, 65, 65, alpha), 1, false);
+	dxDrawText('Health: '..math.floor(localPlayer.health)..', Armor: '..math.ceil(localPlayer.armor), info.x + 60, sy / 2 - 80, sx - 10, sy / 2 - 60, tocolor(255, 255, 255, info.alpha), 1, 'default', 'left', 'center', true, false, false);
+	dxDrawLine(info.x + 55, sy / 2 - 61, sx - 10, sy / 2 - 61, tocolor(65, 65, 65, info.alpha), 1, false);
 
-	dxDrawText('Ping: '..localPlayer.ping..', FPS: '..math.ceil(info.fps), x + 60, sy / 2 - 60, sx - 10, sy / 2 - 40, tocolor(255, 255, 255, alpha), 1, 'default', 'left', 'center', true, false, false);
-	dxDrawLine(x + 55, sy / 2 - 41, sx - 10, sy / 2 - 41, tocolor (65, 65, 65, alpha), 1, false);
+	dxDrawText('Ping: '..localPlayer.ping..', FPS: '..math.ceil(info.fps), info.x + 60, sy / 2 - 60, sx - 10, sy / 2 - 40, tocolor(255, 255, 255, info.alpha), 1, 'default', 'left', 'center', true, false, false);
+	dxDrawLine(info.x + 55, sy / 2 - 41, sx - 10, sy / 2 - 41, tocolor(65, 65, 65, info.alpha), 1, false);
 
-	dxDrawText(math.ceil(((info.velocityVector.x^2 + info.velocityVector.y^2 + info.velocityVector.z^2)^(0.5)) * 161)..' km/h', x + 60, sy / 2 - 40, sx - 10, sy / 2 - 20, tocolor(255, 255, 255, alpha), 1, 'default', 'left', 'center', true, false, false);
-	dxDrawLine(x + 55, sy / 2 - 21, sx - 10, sy / 2 - 21, tocolor (65, 65, 65, alpha), 1, false);
+	dxDrawText(math.ceil(((info.velocityVector.x^2 + info.velocityVector.y^2 + info.velocityVector.z^2)^(0.5)) * 161)..' km/h', info.x + 60, sy / 2 - 40, sx - 10, sy / 2 - 20, tocolor(255, 255, 255, info.alpha), 1, 'default', 'left', 'center', true, false, false);
+	dxDrawLine(info.x + 55, sy / 2 - 21, sx - 10, sy / 2 - 21, tocolor(65, 65, 65, info.alpha), 1, false);
 
-	dxDrawText('$'..localPlayer:getMoney(), x + 60, sy / 2 - 20, sx - 10, sy / 2, tocolor(0, 220, 0, alpha), 1, 'default', 'left', 'center', true, false, false);
-	dxDrawLine(x + 55, sy / 2 - 1, sx - 10, sy / 2 - 1, tocolor (65, 65, 65, alpha), 1, false);
+	dxDrawText('$'..localPlayer:getMoney(), info.x + 60, sy / 2 - 20, sx - 10, sy / 2, tocolor(0, 220, 0, info.alpha), 1, 'default', 'left', 'center', true, false, false);
+	dxDrawLine(info.x + 55, sy / 2 - 1, sx - 10, sy / 2 - 1, tocolor(65, 65, 65, info.alpha), 1, false);
 
-	dxDrawText('Oxygen: '..math.ceil(localPlayer.oxygenLevel/10)..'%', x + 60, sy / 2, sx - 10, sy / 2 + 20, tocolor(255, 255, 255, alpha), 1, 'default', 'left', 'center', true, false, false);
-	dxDrawLine(x + 55, sy / 2 + 19, sx - 10, sy / 2 + 19, tocolor(65, 65, 65, alpha), 1, false);
+	dxDrawText('Oxygen: '..math.ceil(localPlayer.oxygenLevel/10)..'%', info.x + 60, sy / 2, sx - 10, sy / 2 + 20, tocolor(255, 255, 255, info.alpha), 1, 'default', 'left', 'center', true, false, false);
+	dxDrawLine(info.x + 55, sy / 2 + 19, sx - 10, sy / 2 + 19, tocolor(65, 65, 65, info.alpha), 1, false);
 
-	dxDrawText(('You %s be knocked off bike.'):format(localPlayer:canBeKnockedOffBike() and 'can' or 'can\'t'), x + 60, sy / 2 + 20, sx - 10, sy / 2 + 40, tocolor(255, 255, 255, alpha), 1, 'default', 'left', 'center', true, false, false);
-	dxDrawLine(x + 55, sy / 2 + 39, sx - 10, sy / 2 + 39, tocolor(65, 65, 65, alpha), 1, false);
+	dxDrawText(('You %s be knocked off bike.'):format(localPlayer:canBeKnockedOffBike() and 'can' or 'can\'t'), info.x + 60, sy / 2 + 20, sx - 10, sy / 2 + 40, tocolor(255, 255, 255, info.alpha), 1, 'default', 'left', 'center', true, false, false);
+	dxDrawLine(info.x + 55, sy / 2 + 39, sx - 10, sy / 2 + 39, tocolor(65, 65, 65, info.alpha), 1, false);
 
-	dxDrawText(('You %shave a jetpack.'):format(localPlayer:doesHaveJetPack() and '' or 'don\'t '), x + 60, sy / 2 + 40, sx - 10, sy / 2 + 60, tocolor(255, 255, 255, alpha), 1, 'default', 'left', 'center', true, false, false);
-	dxDrawLine(x + 55, sy / 2 + 59, sx - 10, sy / 2 + 59, tocolor(65, 65, 65, alpha), 1, false);
+	dxDrawText(('You %shave a jetpack.'):format(localPlayer:doesHaveJetPack() and '' or 'don\'t '), info.x + 60, sy / 2 + 40, sx - 10, sy / 2 + 60, tocolor(255, 255, 255, info.alpha), 1, 'default', 'left', 'center', true, false, false);
+	dxDrawLine(info.x + 55, sy / 2 + 59, sx - 10, sy / 2 + 59, tocolor(65, 65, 65, info.alpha), 1, false);
 
-	dxDrawText('Interior: '..localPlayer.interior..', Dimension: '..localPlayer.dimension, x + 60, sy / 2 + 60, sx - 10, sy / 2 + 80, tocolor(255, 255, 255, alpha), 1, 'default', 'left', 'center', true, false, false);
-	dxDrawLine(x + 55, sy / 2 + 79, sx - 10, sy / 2 + 79, tocolor(65, 65, 65, alpha), 1, false);
-end;
+	dxDrawText('Interior: '..localPlayer.interior..', Dimension: '..localPlayer.dimension, info.x + 60, sy / 2 + 60, sx - 10, sy / 2 + 80, tocolor(255, 255, 255, info.alpha), 1, 'default', 'left', 'center', true, false, false);
+	dxDrawLine(info.x + 55, sy / 2 + 79, sx - 10, sy / 2 + 79, tocolor(65, 65, 65, info.alpha), 1, false);
+end
 
 addEventHandler('onClientRender', root, drawInfo);
 
 local function showOrHide()
 	info.startTime = getTickCount();
-	showed = not showed;
 
-	if (showed) then
+	if (info.showed) then
 		removeEventHandler('onClientRender',root,showFunction);
 		addEventHandler('onClientRender',root,hideFunction);
 	else
 		addEventHandler('onClientRender',root,showFunction);
 		removeEventHandler('onClientRender',root,hideFunction);
 	end;
-end;
+
+	info.showed = not info.showed;
+end
 
 addEventHandler('onClientResourceStart', resourceRoot, function()
-	bindKey ('i', 'down', showOrHide);
-	info.startTime = getTickCount();
+	bindKey('i', 'down', showOrHide);
 end);
